53976176cd10e1f1bd863f8543e87ea346a5dfb9
HR <hrbuilder@hotmail.com>
2014-01-29 15:19:17 -0500

    Included ngTouch

--- uiDirectives.js
+++ uiDirectives.js
@@ -5,7 +5,7 @@ angular.module('uiDirectives',['ngTouch'])
     template:'<div style="width:100%;height:50px;position:relative;top:0px;left:0px;">' +
                 //'<p ng-repeat="i in metros">{{i.nm}}</p>' +
                 '<div style="width:100%;height:50px;background:blue;position:relative;top:0px;left:0px;">'+
-                  '<p style="color:white">Is this a touch device: {{touch.istouch}}<br />Is this an iOS device: {{touch.isios}}</p>'+
+                  //'<p style="color:white">Is this a touch device: {{touch.istouch}}<br />Is this an iOS device: {{touch.isios}}</p>'+
                 '</div>' + 
              '</div>',
     replace:true,
@@ -26,13 +26,15 @@ angular.module('uiDirectives',['ngTouch'])
         istouch:typeof $window.ontouchmove !== 'undefined' || typeof $window.navigator.maxTouchPoints !== 'undefined'
       }
 
-      if(s.touch.isios || !s.touch.istouch){
+      //TODO: implement a fixed menu on mobile 
+      if(!s.touch.istouch){
         $window.onscroll = metroPos;
       }
-
+      //Option 1: Hide menu and provide option to show -- this would bring onscroll/ontouchmove back into play
+      /*
       if(s.touch.isios){
         $window.ontouchmove = metroPos;
-      }
+      }*/
       
     }
   }  
